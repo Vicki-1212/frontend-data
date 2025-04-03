41cabaef924d2c237f2bda4ebe0f0f2def8c28b9
Vignesh <vicki.k1212@gmail.com>
2025-04-03 19:54:31 +0530

    resolve issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@
 import "./App.css";
 import Form from "./components/Form/Form";
 import HomePage from "./components/HomePage/HomePage";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/cms" element={<Form />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 };
